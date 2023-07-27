7d4b5e45555a211b7b111903c426b9059256fb47
Sebastian <seba.faingerch@gmail.com>
2023-07-27 18:22:28 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HeroesApp } from './HeroesApp';
+import './styles.css';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
